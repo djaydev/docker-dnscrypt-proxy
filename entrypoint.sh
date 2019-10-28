c5a1c1f0678ea4f5bb8642046066257c831d8422
djaydev <33335484+djaydev@users.noreply.github.com>
2019-10-28 16:40:06 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,12 @@ fi
 
 if [ -n "${DNSCRYPT_LISTEN_PORT}" ]
 then
-    su-exec 0 sed -r "s/^(# )?(listen_addresses = ).+$/\2['0.0.0.0:${DNSCRYPT_LISTEN_PORT}']/" -i $CONFIG
+    su-exec 1001 sed -r "s/^(# )?(listen_addresses = ).+$/\2['0.0.0.0:${DNSCRYPT_LISTEN_PORT}']/" -i $CONFIG
 fi
 
 if [ -n "${DNSCRYPT_SERVER_NAMES}" ]
 then
-    su-exec 0 sed -r "s/^(# )?(server_names = ).+$/\2${DNSCRYPT_SERVER_NAMES}/" -i $CONFIG
+    su-exec 1001 sed -r "s/^(# )?(server_names = ).+$/\2${DNSCRYPT_SERVER_NAMES}/" -i $CONFIG
 fi
 
 echo "dnscrypt-proxy -config $CONFIG $@"
