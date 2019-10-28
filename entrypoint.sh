681ae3a5e32d73da0d974fd294a60002a299f72d
djaydev <33335484+djaydev@users.noreply.github.com>
2019-10-28 18:34:32 -0400

    update to 2.0.29

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+# https://gist.github.com/qizhihere/c8db099c673e2f4718418ca05c9b2767
+has_gid () {
+    cut -d: -f1,4 /etc/passwd | grep -q "^${1}:${2}" || return 1
+}
+
+# example: ensure_user btsync 1000 1000
+ensure_user () {
+    local user=$1
+    local uid=$2
+    local gid=$3
+    local group_prefix=${4:-_}
+
+    {
+        deluser $user
+        delgroup $user
+        delgroup $group_prefix$user
+    } 2>/dev/null
+
+    adduser -h / -s /bin/sh -D -u $uid $user
+    has_gid $user $gid || {
+        addgroup -g $gid $group_prefix$user
+        sed -i 's/^\('$user'\(:[^:]\{1,\}\)\{2\}\):[0-9]\{1,\}/\1:'$gid/ /etc/passwd
+    }
+
+}
+
 if [ ! -f /config/dnscrypt-proxy.toml ]
 then
     chown $PUID /usr/share/dnscrypt-proxy/
@@ -19,5 +45,7 @@ then
     sed -r "s/^(# )?(server_names = ).+$/\2${DNSCRYPT_SERVER_NAMES}/" -i $CONFIG
 fi
 
+ensure_user dnscryptproxy $PUID $PGID
+
 echo "dnscrypt-proxy -config $CONFIG $@"
-exec su-exec $PUID dnscrypt-proxy -config $CONFIG $@
+exec su dnscryptproxy dnscrypt-proxy -config $CONFIG $@
